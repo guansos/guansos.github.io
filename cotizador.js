c6462881137382c25055c36fea3033e882c5e49b
guansos <robertoscampos85@hotmail.com>
2022-08-24 19:54:52 -0300

    se utiliza operador ternario.

--- cotizador.js
+++ cotizador.js
@@ -3,11 +3,9 @@ function calcular() {
     var limpiezasAnualesRange = document.getElementById("limpiezasAnualesRange");
     var labelImporteMensual = document.getElementById("importeMensual");
     var labelImporteAnual = document.getElementById("importeAnual");
-    var importeTotal = adultosRange.value * 1000 + limpiezasAnualesRange.value * 337;
-
-    if (adultosRange.value === "0") {
-        importeTotal = 0;
-    }
+    
+    var importeTotal = adultosRange.value === "0" ? 0 :
+        adultosRange.value * 1000 + limpiezasAnualesRange.value * 337;
 
     labelImporteMensual.innerText = importeTotal;
     labelImporteAnual.innerText = importeTotal * 10;
